bb82c50c9e897ee6b7328dab3675ed21ca766670
Beopsik <mbsik6082@naver.com>
2023-02-07 10:35:07 +0900

    Add commitAsync

--- src/main/java/org/example/SimpleConsumer.java
+++ src/main/java/org/example/SimpleConsumer.java
@@ -1,15 +1,14 @@
 package org.example;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Properties;
 
 public class SimpleConsumer {
@@ -24,6 +23,7 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
         consumer.subscribe(Arrays.asList(TOPIC_NAME));
@@ -33,7 +33,7 @@ public class SimpleConsumer {
             for (ConsumerRecord<String, String> record : records){
                 logger.info("{}", record);
             }
-            consumer.commitSync();
+            consumer.commitAsync();
         }
 
 
